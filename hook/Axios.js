8bfd1300443f4f0e170ea75f6f6adf918e2af3c4
S M AL RABBI <95512554+rabbiTheWebDeveloper@users.noreply.github.com>
2023-01-02 19:21:29 +0600

    Update Axios.js

--- hook/Axios.js
+++ hook/Axios.js
@@ -1,10 +1,12 @@
 import Axios from "axios";
+import { shopId } from "../pages/api";
 
 const SuperFetch = Axios.create({
     baseURL: "https://funnelliner.com/api",
     headers: {
         'Content-Type': 'application/json',
-        'X-Requested-With': 'XMLHttpRequest'
+        'X-Requested-With': 'XMLHttpRequest',
+         shop_id :shopId
     }
 })
 export default SuperFetch
